fe07fe74cfddfe4ccf33b6bec96bbc988faa412d
Daniel Avila <rdavilao@users.noreply.github.com>
2023-05-17 09:47:01 +0200

    Update entrypoint.sh

--- satori-action-testing/entrypoint.sh
+++ satori-action-testing/entrypoint.sh
@@ -8,11 +8,17 @@ echo "::group::Rasa train"
 echo "Running 'rasa train' command"
 rasa train
 echo "::endgroup::"
-rasa run &
+rasa run --enable-api &
 echo "Rasa run"
 
-mkdir botium-test
-cd botium-test
+if [ -d "/botium-test" ]; then
+    echo "Existe"
+else
+    echo "No existe"
+    mkdir botium-test
+    cd botium-test
+fi
+
 botium-cli init
 botiumOutput=$(botium-cli run 2>&1)
 
